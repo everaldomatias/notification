d320e4c77a7449de2a816d44f6f57543ffd685b8
Everaldo Matias <matiaseveraldo@gmail.com>
2021-01-31 23:02:24 -0300

    Update library parameters

--- _app/Email.php
+++ _app/Email.php
@@ -10,23 +10,23 @@ class Email
 
     private $mail = \stdClass::class;
 
-    public function __construct()
+    public function __construct($smtpDebug, $host, $user, $pass, $smtpSecure, $port, $setFromEmail, $setFromName)
     {
 
-        // Server settings
         $this->mail = new PHPMailer(true);
-        $this->mail->SMTPDebug = 2;
+        $this->mail->SMTPDebug  = $smtpDebug;
         $this->mail->isSMTP();
-        $this->mail->Host       = 'host.com';
+        $this->mail->Host       = $host;
         $this->mail->SMTPAuth   = true;
-        $this->mail->Username   = 'email';
-        $this->mail->Password   = 'password';
-        $this->mail->SMTPSecure = 'tls';
-        $this->mail->Port       = 587;
-        $this->mail->CharSet = 'utf-8';
+        $this->mail->Username   = $user;
+        $this->mail->Password   = $pass;
+        $this->mail->SMTPSecure = $smtpSecure;
+        $this->mail->Port       = $port;
+        $this->mail->CharSet    = 'utf-8';
         $this->mail->setLanguage('br');
         $this->mail->isHTML();
-        $this->mail->setFrom('email@email.com', 'Teste Local Envio de Email');
+        $this->mail->setFrom($setFromEmail, $setFromName);
+
     }
 
     public function sendMail($subject, $body, $replyEmail, $replayName, $addressEmail, $addressName)
---
+++ test/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '/../libs/autoload.php';
+
+use Notification\Email;
+
+$mail = new Email(2, "mail.host.com", "your@email.com", "your-pass", "smtp secure (tls / ssl)", "port (587)", "from@email.com", "From Name");
+
+$email->sendEmail("Subject", "Content", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
+
+var_dump($email);
